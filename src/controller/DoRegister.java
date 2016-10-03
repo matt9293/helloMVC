03b689846d0c80cce878a7e82869457c297ee924
younguk <matt9293@gmail.com>
2016-10-03 17:00:49 +0900

    Create New Project

---
+++ src/controller/DoRegister.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Customer;
+import service.CustomerService;
+
+/**
+ * Servlet implementation class DoRegister
+ */
+@WebServlet("/doRegister")
+public class DoRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoRegister() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String gender = request.getParameter("gender");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		
+		CustomerService service = (CustomerService) CustomerService.getInstance();
+		service.addCustomer(new Customer(id, password, name, gender, email));
+		
+	    RequestDispatcher dispatcher = request.getRequestDispatcher("/view/registerSuccess.jsp");
+		dispatcher.forward(request, response);
+	
+
+	}
+}
